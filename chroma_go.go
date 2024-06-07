0c006756d61312ede64a0e956c95218b316d925f
EyobAshenaki <ethioeyoba@gmail.com>
2024-06-07 13:30:54 +0300

    chore: add chroma client/server test code

--- chroma_go.go
+++ chroma_go.go
@@ -1,7 +1,6 @@
 package chroma_go
 
 import (
-	"context"
 	"fmt"
 
 	chroma "github.com/amikos-tech/chroma-go"
@@ -10,23 +9,41 @@ import (
 func Connect() string {
 	fmt.Println("Connecting to Chroma...")
 
-	client, err := chroma.NewClient("localhost:8000")
+	client, connectionErr := chroma.NewClient("http://localhost:8001")
 
-	if err != nil {
-		fmt.Println("Error connecting to Chroma:", err)
+	if connectionErr != nil {
+		fmt.Println("Error connecting to Chroma:", connectionErr)
 		return "Error connecting to Chroma"
 	}
 
 	fmt.Println("Connected to Chroma:", client)
 
-	// ctx := context.TODO()
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, "name", "eyob")
-	dosth(ctx)
+	// // ctx := context.TODO()
+	// ctx := context.Background()
+	// // ctx = context.WithValue(ctx, "name", "eyob")
+	// // dosth(ctx)
+
+	// count, err := client.CountCollections(ctx)
+
+	// if err != nil {
+	// 	fmt.Println("Error Connection Count:", err)
+	// 	return "Error Connection Count"
+	// }
+
+	// fmt.Println("Connected to Chroma:", count)
+
+	// res, err := client.Heartbeat(ctx)
+
+	// if err != nil {
+	// 	fmt.Println("Error Heartbeat:", err)
+	// 	return "Error Heartbeat"
+	// }
+
+	// fmt.Println("Connected to Chroma:", res["nanosecond heartbeat"])
 
 	return "Connected"
 }
 
-func dosth(ctx context.Context) {
-	fmt.Printf("Doing something! My name is %s\n", ctx.Value("name"))
-}
+// func dosth(ctx context.Context) {
+// 	fmt.Printf("Doing something! My name is %s\n", ctx.Value("name"))
+// }
