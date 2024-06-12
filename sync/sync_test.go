ad7e1996129478f9962af0428a5c5e95afdd4e35
EyobAshenaki <ethioeyoba@gmail.com>
2024-06-12 05:56:08 +0300

    test: add test code to start method in the sync module

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -4,25 +4,36 @@ import (
 	"fmt"
 	"testing"
 
-	sync "github.com/EyobAshenaki/chroma-go/sync"
+	"github.com/EyobAshenaki/chroma-go/sync"
 )
 
 func TestSyncStart(t *testing.T) {
-	// var wg sync.WaitGroup
-	// wg.Add(1)
-
-	// go func() {
-	// 	defer wg.Done()
-	// 	fmt.Println("Hello")
-	// }()
-
-	// wg.Wait()
-
 	fmt.Println("Sync test start")
+
+	sync.Init()
 
 	if err := sync.Start(); err != nil {
 		t.Errorf("Start sync error: %s", err)
 	}
+
+	sync.Close()
+
+	// if _, err := sync.GetAllChannels(); err != nil {
+	// 	t.Errorf("Start sync error: %s", err)
+	// }
+
+	// postParams := url.Values{
+	// 	"since": {""},
+	// }
+
+	// postParams.Set("since", "1717873778562")
+
+	// fmt.Println("Param: ", postParams)
+	// channelId := "znqgwqwykb887k9zpn7z1m47mc"
+
+	// if _, err := sync.FetchPostsForPage(channelId, postParams); err != nil {
+	// 	t.Errorf("Start sync error: %s", err)
+	// }
 
 	fmt.Println("Sync test end")
 }
