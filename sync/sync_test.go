af51402ff928cc7b14713ef5b6393cfa0e0dc8c4
EyobAshenaki <ethioeyoba@gmail.com>
2024-06-29 15:04:57 +0300

    test: implement a few http routes for testing
    - Implemented /sync/is_fetch_in_progress route
    - Implemented /sync/is_sync_in_progress route
    - Implemented /sync/fetch_interval route for getting fetch interval using GET method
    and updating fetch interval using POST method
    - Implemented /sync/reset route to test reset data feature

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -3,11 +3,16 @@ package sync_test
 import (
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	sse "github.com/EyobAshenaki/chroma-go/server_side_event"
+	"github.com/EyobAshenaki/chroma-go/sync"
+	"github.com/rs/cors"
 )
 
 func TestSyncStart(t *testing.T) {
@@ -33,14 +38,115 @@ func TestSyncStart(t *testing.T) {
 	// }
 
 	// ----------------------------------------------
+	mux := http.NewServeMux()
 
 	broker := sse.NewSSEServer()
 
-	http.Handle("/sync/start", broker)
+	mux.Handle("/sync/start", broker)
 
-	http.Handle("/sync/stop", broker)
+	mux.Handle("/sync/stop", broker)
 
-	err := http.ListenAndServe(":3333", nil)
+	mux.HandleFunc("/sync/is_fetch_in_progress", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		mmSync := sync.GetSyncInstance()
+
+		isFetchInProgress, err := mmSync.GetIsFetchInProgress()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		io.Writer.Write(w, []byte(strconv.FormatBool(isFetchInProgress)))
+	})
+
+	mux.HandleFunc("/sync/is_sync_in_progress", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		mmSync := sync.GetSyncInstance()
+
+		isSyncInProgress, err := mmSync.GetIsSyncInProgress()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		io.Writer.Write(w, []byte(strconv.FormatBool(isSyncInProgress)))
+	})
+
+	mux.HandleFunc("/sync/fetch_interval", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" && r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		mmSync := sync.GetSyncInstance()
+
+		if r.Method == "GET" {
+			fetchInterval, err := mmSync.GetFetchInterval()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			io.Writer.Write(w, []byte(strconv.Itoa(fetchInterval)))
+		}
+
+		if r.Method == "POST" {
+			hasFetchInterval := r.URL.Query().Has("fetch_interval")
+			if !hasFetchInterval {
+				http.Error(w, "fetch_interval query field not found", http.StatusBadRequest)
+				return
+			}
+
+			fetchInterval := r.URL.Query().Get("fetch_interval")
+			if fetchInterval == "" {
+				http.Error(w, "fetch_interval query field is empty", http.StatusBadRequest)
+				return
+			}
+
+			fetchIntervalInt, parseError := strconv.Atoi(fetchInterval)
+			if parseError != nil {
+				http.Error(w, parseError.Error(), http.StatusBadRequest)
+				return
+			}
+
+			fetchDuration := time.Duration(fetchIntervalInt) * time.Hour
+
+			err := mmSync.UpdateFetchInterval(fetchDuration)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			io.Writer.Write(w, []byte("Fetch interval updated successfully"))
+		}
+	})
+
+	mux.HandleFunc("/sync/reset", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "DELETE" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		isReset, err := sync.ResetVectorStore()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		io.Writer.Write(w, []byte(fmt.Sprintf("Vector store reset successfully: %v", isReset)))
+	})
+
+	handler := cors.Default().Handler(mux)
+
+	err := http.ListenAndServe(":3333", handler)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
