b2787e684c1512d2d73678ffb749999fcd7817cf
EyobAshenaki <ethioeyoba@gmail.com>
2024-07-03 18:38:35 +0300

    chore: experiment with log colors

--- chroma_go_test.go
+++ chroma_go_test.go
@@ -1,15 +1,14 @@
 package chroma_go_test
 
 import (
+	"log"
 	"testing"
-
-	chroma_go "github.com/EyobAshenaki/chroma-go"
 )
 
 func TestChromaGo(t *testing.T) {
 	t.Log("Test start")
 
-	client := chroma_go.GetChromaInstance()
+	// client := chroma_go.GetChromaInstance()
 
 	// collection, colError := client.GetOrCreateCollection("mattermost")
 	// if colError != nil {
@@ -28,12 +27,28 @@ func TestChromaGo(t *testing.T) {
 
 	// ----------------------------------------------------
 
-	mmChannelIds := []interface{}{
-		// "9unddga5zin75goadjbwipz9tr",
-		// "jdwhwtegcpb3tei871ohw9sz9y",
-	}
+	// mmChannelIds := []interface{}{
+	// 	// "9unddga5zin75goadjbwipz9tr",
+	// 	// "jdwhwtegcpb3tei871ohw9sz9y",
+	// }
 
-	client.Query("testing", mmChannelIds)
+	// client.Query("testing", mmChannelIds)
+
+	// ------------------------------------------
+
+	var Reset = "\033[0m"
+	var Red = "\033[31m"
+	// var Green = "\033[32m"
+	// var Yellow = "\033[33m"
+	var Blue = "\033[34m"
+	// var Purple = "\033[35m"
+	// var Cyan = "\033[36m"
+	// var Gray = "\033[37m"
+	// var White = "\033[97m"
+
+	// Reset helps so that the color doesn't bleed in to the next print statement
+	println(Red + "This is Blue" + Reset)
+	log.Println(Blue + "This is Blue" + Reset)
 
 	t.Log("Test end")
 }
